23d5a2a5a20e6570cab7a8c086bd9d525205957f
Gunnar Andersson <gand@acm.org>
2015-09-29 22:49:03 +0200

    Scripts and example files added

---
+++ build-commonapi.sh
@@ -0,0 +1,218 @@
+#!/bin/sh -x
+
+# This script is based on the detailed instructions from GENIVI public wiki
+# written by Juergen Gehring.
+# "CommonAPI C++ D-Bus in 10 minutes (from scratch)"
+# https://genivi-oss.atlassian.net/wiki/pages/viewpage.action?pageId=5472316
+# FIXME: Update URL when the wiki domain changes
+#
+# (C) Gunnar Andersson <gand@acm.org>
+# Purpose: Download and native compilation of CommonAPI C++ DBus
+#
+# License: http://creativecommons.org/licenses/by-sa/4.0/
+# ( since the material was taken from GENIVI Public Wiki:
+#  "Except where otherwise noted, content on this site is licensed under a
+#  Creative Commons Attribution-ShareAlike 4.0 International License" )
+
+
+# According to web page:
+# "Valid for CommonAPI 3.1.3 and vsomeip 1.3.0"
+
+# Get absolute path to base dir
+MYDIR=$(dirname "$0")
+cd "$MYDIR"
+BASEDIR="$PWD"
+
+try() { $@ || fail "Command $* failed -- check above for details" ;}
+
+fail() {
+   set +x # Turn off command listing now, if it's on
+   echo "FAILED!  Message follows:"
+   echo $@
+   echo "Halted, hit return to continue, or give up..."
+   read x
+
+}
+
+git_clone() {
+   # This is so we don't fail if directory already exists
+   # but still, if a new clone is attempted and fails, then fail
+   d="$(basename $1)" # repo/directory name
+   d="${d%.git}"      # Strip off ".git" if it is there
+   if [ -d $d ] ; then
+      echo "Directory $d exists, no git clone attempted"
+   else
+      try git clone $1 
+   fi
+}
+
+check_expected() {
+for f in $@ ; do 
+   [ -e $f ] || fail "Expected result file $f not present (not built)!"
+done
+}
+
+install_prerequisites() {
+  sudo yum install expat-devel cmake gcc g++ autotools
+}
+
+apply_patch() {
+   patch -p1 <"$1" || fail "patch application failed -- see above"
+}
+
+install_prerequisites
+
+# Build Common API C++ Runtime
+cd "$BASEDIR" || fail
+git_clone http://git.projects.genivi.org/ipc/common-api-runtime.git
+cd common-api-runtime/ || fail
+mkdir -p build
+cd build/ || fail
+try cmake ..
+try make -j4
+check_expected libCommonAPI.so
+
+# Build Common API C++ DBus Runtime
+cd "$BASEDIR" || fail
+git_clone http://git.projects.genivi.org/ipc/common-api-dbus-runtime.git
+try wget -c http://dbus.freedesktop.org/releases/dbus/dbus-1.8.20.tar.gz
+try tar -xzf dbus-1.8.20.tar.gz
+cd dbus-1.8.20/ || fail
+apply_patch ../common-api-dbus-runtime/src/dbus-patches/capi-dbus-add-send-with-reply-set-notify.patch
+apply_patch ../common-api-dbus-runtime/src/dbus-patches/capi-dbus-add-support-for-custom-marshalling.patch
+apply_patch ../common-api-dbus-runtime/src/dbus-patches/capi-dbus-correct-dbus-connection-block-pending-call.patch
+try ./configure
+try make -j4
+check_expected dbus/.libs/libdbus-1.so.3
+
+# Build Common API DBus
+cd "$BASEDIR" || fail
+cd common-api-dbus-runtime/ || fail
+mkdir -p build
+cd build || fail
+export PKG_CONFIG_PATH="$BASEDIR/dbus-1.8.20"
+try cmake -DUSE_INSTALLED_COMMONAPI=OFF -DUSE_INSTALLED_DBUS=OFF ..
+try make -j4
+check_expected libCommonAPI-DBus.so
+
+# Create application
+cd "$BASEDIR" || fail
+mkdir project
+cd project/ || fail
+mkdir fidl
+cp "$BASEDIR/examples/HelloWorld.fidl" fidl/
+
+# Ready! A service which instantiates the interface HelloWorld provides the
+# function sayHello which can be called. The next step is to generate code.
+# For that we need the code generators. We copy them into the new
+# subdirectory cgen of our project directory:
+
+# Get Code Generators
+cd "$BASEDIR/project" || fail
+mkdir -p cgen
+cd cgen/ || fail
+try wget http://docs.projects.genivi.org/yamaica-update-site/CommonAPI/generator/3.1/3.1.3/commonapi-generator.zip
+try wget http://docs.projects.genivi.org/yamaica-update-site/CommonAPI/generator/3.1/3.1.3/commonapi_dbus_generator.zip
+try unzip commonapi-generator.zip -d commonapi-generator
+try unzip commonapi_dbus_generator.zip -d commonapi_dbus_generator
+try chmod +x ./commonapi-generator/commonapi-generator-linux-x86
+try chmod +x ./commonapi_dbus_generator/commonapi-dbus-generator-linux-x86
+
+# Now you find the executables of the code generators in
+# cgen/commonapi-generator and cgen/commonapi_dbus_generator, respectively.
+# There are four versions (Linux, Windows and 64bit variants). Type uname
+# -m if you don't know if you have a 32bit or 64bit version of Linux (you
+# get i686 or x86_64). For the further description we assume that you have
+# the 32bit version.
+# Do not complain about details, such as that you have to call chmod or that
+# the names contain sometimes underscores and sometimes hyphen. It will
+# change in future versions.
+
+#Finally you can generate code (CommonAPI code with the commonapi-generator and CommonAPI D-Bus code with the commonapi-dbus-generator):
+#Generate Code
+cd "$BASEDIR/project" || fail
+try ./cgen/commonapi-generator/commonapi-generator-linux-x86 -sk ./fidl/HelloWorld.fidl
+try ./cgen/commonapi_dbus_generator/commonapi-dbus-generator-linux-x86 ./fidl/HelloWorld.fidl
+cd src-gen/v1_0/commonapi || fail
+
+check_expected HelloWorldDBusDeployment.cpp HelloWorldDBusProxy.cpp\
+      HelloWorldDBusStubAdapter.cpp HelloWorld.hpp HelloWorldProxy.hpp\
+      HelloWorldStubDefault.hpp HelloWorldDBusDeployment.hpp\
+      HelloWorldDBusProxy.hpp  HelloWorldDBusStubAdapter.hpp\
+      HelloWorldProxyBase.hpp  HelloWorldStubDefault.cpp  HelloWorldStub.hpp
+cd - || fail
+
+#If everything worked, the generated code will be in the new directory src-gen. The option -sk generates a default implementation of your interface instance in the service.
+#Step 5: Write the client and the service application
+check_expected src-gen/
+
+# Now we can start to write the Hello World application. Create new subdirectories src and build in the project directory and change to src.
+# Create src and build directories
+cd "$BASEDIR/project" || fail
+mkdir src
+mkdir build
+check_expected build  cgen  fidl  src  src-gen
+
+# Now we have to create 4 files: The client code (HelloWorldClient.cpp), one file for the main-function of the service (HelloWorldService.cpp) and 2 files (header and source) for the implementation of the generated skeleton for the stub (we call it HelloWorldStubImpl.hpp and HelloWorldStubImpl.cpp).
+
+# Let's begin with the client. Create a new file HelloWorldClient.cpp with the editor of your choice and type:
+
+try cp "$BASEDIR/examples/HelloWorldClient.cpp" src/
+
+#At the beginning of each CommonAPI application it is necessary to get a pointer to the generic runtime object. The runtime is necessary to create proxies and stubs. A client application has to call functions of an instance of an interface in the service application. In order to be able to call these functions we must build a proxy for this interface in the client. The interface name is the template parameter of the buildProxy method; furthermore we build the proxy for a certain instance; the instance name is the second parameter in the buildProxy method. In principle there is the possibility to distinguish between instances in different so-call domains (first parameter), but we don't want to discuss this in depth at the moment and take always the domain "local".
+
+#The proxy provides the API function isAvailable; if the we start the service first then isAvailable returns always true. It is also possible to register a callback which is called when the service becomes available; but we try to keep it here as simple as possible.
+
+#Now we call the function sayHello which we have defined in our fidl-file. The function has one in-parameter (string) and one out-parameter (also string). Have a look into HelloWorldPorxy.hpp to get the information how exactly the function sayHello must be called. Here it is important to know that it is possible to call the synchronous variant of this function (what we do here) or to call the asynchronous variant (sayHelloAsync) which is slightly more complicated. One return value is the so-called CallStatus, which gives us the information if the call was successful or not. Again, to keep it simple we do not check the CallStatus and hope that everthing worked fine.
+
+#We continue now to write the service. Create a new file HelloWorldService.cpp:
+try cp "$BASEDIR/examples/HelloWorldService.cpp" src/
+
+# The main function of the service is even simpler as the main function of
+# the client because the implementation of the interface functions is in
+# the stub implementation. Again we need the pointer to the runtime
+# environment; then we instantiate our implementation of the stub and
+# register this instance of the interface by calling registerService with
+# an instance name. The service shall run forever and answer to function
+# calls until it becomes killed; therefore we need the while loop at the
+# end of the main function.
+
+# At the end we need the stub implementation; we realize it by creating a
+# stub-implementation class which is inherited from the stub-default
+# implementation. The header file is: 
+
+try cp "$BASEDIR/examples/HelloWorldStubImpl.hpp" src/
+try cp "$BASEDIR/examples/HelloWorldStubImpl.cpp" src/
+
+# If the function sayHello is called it gets the name (which is supposed to
+# be the name of the developer of this application) and returns it with an
+# added "Hello" in front. The return parameter is not directly the string
+# as it is defined in the fidl-file; it is a standard function object with
+# the return parameters as in-parameters. The reason for this is to provide
+# the possibility to answer not synchronously in the implementation of this
+# function but to delegate the answer to a different thread.
+
+# Step 6: Build and run
+
+# Since you must have installed CMake in order to build the CommonAPI
+# runtime libraries, the fastest way to compile and build our applications
+# is to write a simple CMake file. Create a new file CMakeLists.txt
+# directly in the project directory: CMakeLists.txt
+
+try cp "$BASEDIR/examples/CMakeLists.txt" .
+
+# As include paths we need the include directories of CommonAPI and bindings and D-Bus; the directory of the generated code must be also added. We link everything together and do not discuss here questions concerning the configuration with different bindings. Therefore we tell CMake where to find the CommonAPI libraries and libdbus (replace the absolute paths with the paths on your machine). At the end we build two executables, one for the service and one for the client.
+# Now call CMake (remember that we created the build directory before:
+# Build everything
+cd "$BASEDIR/project" || fail
+cd build || fail
+try cmake ..
+try make -j4
+
+# Your output should look similiar. In the build direcory there should be two executables now: HelloWorldClient and HelloWorldService.
+
+echo "You may now run $PWD/HelloWorldService &"
+echo "and $PWD/HelloWorldClient"
+# ./HelloWorldService &
+# ./HelloWorldClient
+
---
+++ test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# A very rudimentary automated test - run the generated and compiled hello
+# world binaries and see that they are communicating. Return the standard
+# result codes:
+# 0 = success,
+# non-zero = failure
+
+# Get a fixed point in the directories (this script's directory)
+D=$(dirname "$0")
+cd "$D"
+MYDIR="$PWD"
+
+client="$MYDIR/project/build/HelloWorldClient"
+server="$MYDIR/project/build/HelloWorldService"
+
+[ -x "$client" ] || { echo "$0 : Executable not found ($client)" ; exit 1 ; }
+[ -x "$server" ] || { echo "$0 : Executable not found ($server)" ; exit 1 ; }
+
+stop_server() { kill $server_pid ;}
+success() { echo Success ; stop_server ; exit 0 ;}
+
+# Run server
+$server &
+server_pid=$!
+echo server_pid=$server_pid
+
+# Run client in background check for messages
+exec 3< <($client 2>/dev/null)
+
+# Give the process up to 3 seconds to succeed
+for x in 1 2 3 ; do
+   read -t 1 line <&3
+   echo "$line"
+   echo "$line" | egrep -q "Got message:.*Hello Bob" && success
+done
+
+# Timed out
+echo "HelloWorldClient failed sending/receiving"
+exit -1
+
+
