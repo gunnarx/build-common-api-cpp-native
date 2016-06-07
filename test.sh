e522bae167a7800569325cfd63186136bd95d9ed
Gunnar Andersson <gand@acm.org>
2016-06-07 17:59:27 +0200

    Make it nicer and add Copyright, license etc.

--- test.sh
+++ test.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+# ------------------------------------------------------------------------
+# Common-API test script
+# (C) 2016 Gunnar Andersson
+# License: CC-BY-SA-4.0 or MPLv2 or GPLv2 or GPLv3 (your choice)
+# ------------------------------------------------------------------------
 
 # A very rudimentary automated test - run the generated and compiled hello
 # world binaries and see that they are communicating. Return the standard
 # result codes:
 # 0 = success,
 # non-zero = failure
+#
+# (First you must have run build-commonapi.sh successfully)
+# ------------------------------------------------------------------------
 
 # Get a fixed point in the directories (this script's directory)
 D=$(dirname "$0")
