e5b44b72bbf39185a0ef45f4547b3a91c2de804e
Gunnar Andersson <gandersson@genivi.org>
2018-02-27 00:03:53 +0100

    NULL check for HelloWorldClient proxy
    I ran into this a few times with older versions. It can happen if
    there's a version mismatch but I seemed to get it also after rebuilding
    all with one version.  In any case, it went away with 3.1.10. This is
    just some code to write a message instead of segfaulting on the pointer
    dereference.
    
    Other than that, I've tried to do minimal changes (so far) to the
    example as it is written in Wiki, only encoding the steps in the script.
    
    Signed-off-by: Gunnar Andersson <gandersson@genivi.org>

--- examples/HelloWorldClient.cpp
+++ examples/HelloWorldClient.cpp
@@ -10,6 +10,17 @@ int main() {
     std::shared_ptr < CommonAPI::Runtime > runtime = CommonAPI::Runtime::get();
     std::shared_ptr<HelloWorldProxy<>> myProxy = runtime->buildProxy<HelloWorldProxy>("local", "test");
 
+    // START modification
+    // I ran into a NULL myProxy on older versions (possibly a version
+    // mismatch).  Instead of segfaulting, let's note it here.  This is the
+    // only change compared to the original "10 minutes" example
+    if (!myProxy)
+    {
+       std::cerr << "HelloWorldClient FAIL: Returned  proxy is NULL! - Giving up!\n";
+       return 2;
+    }
+    // END modification
+
     std::cout << "Checking availability!" << std::endl;
     while (!myProxy->isAvailable())
         usleep(10);
