47017403bdb22666c5e4667e9a6189100c97660c
Gunnar Andersson <gandersson@genivi.org>
2019-07-01 23:55:23 +0200

    run-in-docker: Support --rm optionally, and give better instructions
    Signed-off-by: Gunnar Andersson <gandersson@genivi.org>

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Support --rm flag optionally
+rmflag=
+[ "$1" = "--rm" ] && rmflag="--rm"
+
+PROJ_URL="https://github.com/gunnarx/build-common-api-cpp-native"
 DISTRO=ubuntu:16.04  # This has java version 8 by default
 JAVA_PACKAGE=openjdk-8-jre
 
@@ -8,6 +13,9 @@ echo This will run the script in a controlled docker container environment
 # If you wish to play around with the result the --rm (remove) flag
 # should be avoided.
 set -x
-docker run --rm -i $DISTRO \
-  bash -c "apt-get update ; apt-get install -y git $JAVA_PACKAGE ; git clone https://github.com//gunnarx/build-common-api-cpp-native ; cd build-common-api-cpp-native ; ./build-commonapi.sh"
+docker run $rmflag -i $DISTRO \
+  bash -c "apt-get update ; apt-get install -y git $JAVA_PACKAGE ; git clone $PROJ_URL ; cd build-common-api-cpp-native ; ./build-commonapi.sh"
 
+echo "---"
+echo "Container has now exited.  To look at the results, find it among docker ps (not running), start it and then exec a shell in the container."
+echo
