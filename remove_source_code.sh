8da2fa2024a843ad499e3e57c10c26988cc8ca5b
Gunnar Andersson <gandersson@genivi.org>
2020-07-01 13:52:37 +0200

    Add script to remove source code (for docker image)

---
+++ remove_source_code.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Make sure we are in project root first
+cd "$(dirname "$0")"
+
+rm -rf boost_* capicxx-core-runtime capicxx-dbus-runtime capicxx-someip-runtime dbus-* vsomeip patch
+
